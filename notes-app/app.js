b0ae2cdd7c6233f956a68e16a667a2a016798dc4
makaranddhamale <makarand.dhamale09@gmail.com>
2024-12-06 19:35:39 +0530

    Add command line configuration

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,47 @@
 import yargs from "yargs";
 import {hideBin} from "yargs/helpers";
 
-yargs(hideBin(process.argv)).version("1.1.0").argv()
+const yargsConfig = yargs(hideBin(process.argv))
+    .version("1.1.0")
+    .command({
+        command: "add",
+        describe: "Add a new note!!",
+        builder: {
+            title: {
+                describe: "Note title",
+                demandOption: true,
+                type: "string"
+            },
+            body: {
+                describe: "Note body",
+                demandOption: true,
+                type: "string"
+            }
+        },
+        handler: function (argv) {
+            console.log("Title : ", argv.title)
+            console.log("Body : " + argv.body)
+        }
+    })
+    .command({
+        command: "remove",
+        describe: "Removing a note!!",
+        handler: function () {
+            console.log("Removing a note!!")
+        }
+    })
+    .command({
+        command: "list",
+        describe: "List all the notes",
+        handler: function () {
+            console.log("Listing all the notes!!")
+        }
+    })
+    .command({
+        command: "read",
+        describe: "Read the note",
+        handler: function () {
+            console.log("Reading the note!!")
+        }
+    })
+    .argv
