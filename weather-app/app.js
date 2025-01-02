e2664641e4d822580e0be66cc209dc25fca8290f
makaranddhamale <makarand.dhamale09@gmail.com>
2025-01-02 18:33:20 +0530

    Added log for weater app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const url =
+  "http://api.weatherstack.com/current?access_key=64f2540d1daa2dd1c8184268596b8532&query='Pune'";
 
-const url = "http://api.weatherstack.com/current?access_key=64f2540d1daa2dd1c8184268596b8532&query='Pune'"
-
-axios.get(url).then(response => {
-    console.log(response.data)
-    console.log(response)
-})
+axios.get(url).then((response) => {
+  const current = response.data.current;
+  console.log(
+    `${current.weather_descriptions[0]},It is currently ${current.temperature}°C. It feels like ${current.feelslike}°C`
+  );
+});
