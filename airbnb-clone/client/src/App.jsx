9afd50e3ada99803b2646be1625086724d3f5490
makaranddhamale <makarand.dhamale09@gmail.com>
2025-02-21 20:11:14 +0530

    Header and layout added

--- airbnb-clone/client/src/App.jsx
+++ airbnb-clone/client/src/App.jsx
@@ -1,49 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div>
-      <header className="p-4">
-        <a href="" className="flex items-center gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-8 -rotate-90"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-            />
-          </svg>
-          <span className="font-bold text-xl">airbnb</span>
-        </a>
-        <div>
-          <div>Anywhere</div>
-          <div>Any week</div>
-          <div>Any guests</div>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-          </button>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+    </Routes>
   );
 }
 
