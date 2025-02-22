53929e2f12a0fc859b7bda51e7a51dab1422759e
makaranddhamale <makarand.dhamale09@gmail.com>
2025-02-22 23:02:02 +0530

    Added docker compose file

--- airbnb-clone/api/src/index.ts
+++ airbnb-clone/api/src/index.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import cors from "cors";
+import { RegisterRequest } from "./model";
 
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/test", (req, res) => {
   res.json("test ok");
+});
+
+app.post("/register", (req, res) => {
+  const request: RegisterRequest = req.body;
+  res.json(request);
 });
 
 app.listen(PORT, () => {
